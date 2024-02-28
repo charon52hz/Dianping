26d459a6cdde3ceef4e0c2c93a325569f41962b3
charon52hz <charon52hz@gmail.com>
2024-02-28 14:59:23 +0800

    登录流程完整实现

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -2,15 +2,20 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -40,5 +45,37 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         log.debug("发送短信验证码成功，验证码：{}",code);
         //6.返回ok
         return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        String phone = loginForm.getPhone();
+        String code = loginForm.getCode();
+
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号码格式错误");
+        }
+
+        String sessionCode = (String) session.getAttribute("code");
+        if (sessionCode == null || !sessionCode.equals(code)){  //验证码过期或不一致
+            return Result.fail("验证码错误");
+        }
+
+        User user = query().eq("phone", phone).one();
+        if (user == null){
+            user = createUserWithPhone(phone);
+        }
+        UserDTO userDTO = new UserDTO();
+        BeanUtils.copyProperties(user,userDTO);
+        session.setAttribute("user",userDTO);
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX+RandomUtil.randomString(6));
+        save(user);
+        return user;
     }
 }
